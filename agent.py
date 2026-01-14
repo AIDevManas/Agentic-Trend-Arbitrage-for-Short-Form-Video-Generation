7b232f6d56512fdb21b14d2cf27c55705c3893ff
AIDevManas <maans.gaurkar.dev@gmail.com>
2026-01-14 21:36:47 +0530

    Two agent done

--- agent.py
+++ agent.py
@@ -4,8 +4,14 @@ from crewai_tools import YoutubeVideoSearchTool
 from dotenv import load_dotenv
 
 
-os.environ["OPENROUTER_API_KEY"] = "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"
-os.environ["OPENAI_API_KEY"] = "sk-dummy"
+load_dotenv()
+
+if not os.getenv("OPENROUTER_API_KEY"):
+    raise ValueError("OPENROUTER_API_KEY not found in environment variables")
+
+
+os.environ["OPENAI_API_KEY"] = os.environ["OPENROUTER_API_KEY"]
+os.environ["OPENAI_API_BASE"] = "https://openrouter.ai/api/v1"
 
 
 llm = LLM(
@@ -20,32 +26,52 @@ llm = LLM(
 
 yt_tools = YoutubeVideoSearchTool()
 
+
+
 researcher = Agent(
-    role = "Youtube Trend Researcher",
-    goal = "Find viral angles and key facts about {topic} for a YouTube Short",
-    backstory = ("You are an expert at analyzing YouTube content. "
-        "You know exactly what makes a video go viral. "
-        "You dig deep into videos to find surprising facts that grab attention."),
-    tools = [yt_tools],
-    llm = llm,
-    verbose = True,
-    allow_delegations=False
-)
+        role = "Youtube Trend Researcher",
+        goal = "Find viral angles and key facts about {topic} for a YouTube Short",
+        backstory = ("You are an expert at analyzing YouTube content. "
+            "You know exactly what makes a video go viral. "
+            "You dig deep into videos to find surprising facts that grab attention."),
+        tools = [yt_tools],
+        llm = llm,
+        verbose = True,
+        allow_delegations=False
+    )
+
+    
 
 research_task = Task(
-    description=("1. Search YouTube for videos related to: {topic}. "
-        "2. Extract 3 shocking facts or 'hidden gems' from these videos. "
-        "3. Identify the common 'hook' used in popular videos on this topic."),
-    expected_output="A bullet-point list of 3 facts and 1 recommended hook strategy.",
-    agent=researcher
+        description=("1. Search YouTube for videos related to: {topic}. "
+            "2. Extract 3 shocking facts or 'hidden gems' from these videos. "
+            "3. Identify the common 'hook' used in popular videos on this topic."),
+        expected_output="A bullet-point list of 3 facts and 1 recommended hook strategy.",
+        agent=researcher
+    )
+
+scriptwriter = Agent(
+    role='Shorts Scriptwriter',
+    goal='Write a punchy, under-60-second script',
+    backstory="You are a viral copywriter. You write fast-paced scripts with strong hooks.",
+    llm=llm,
+    verbose=True
+)
+
+
+script_task = Task(
+    description="Write a 60-second YouTube Short script using the research. Include visual cues [Visual: ...].",
+    expected_output="A full script with Hook, Body, and Outro.",
+    agent=scriptwriter,
+    context=[research_task]
 )
 
 crew = Crew(
-    agents=[researcher],
-    tasks=[research_task]
-)
+        agents=[researcher,scriptwriter],
+        tasks=[research_task,script_task]
+    )
 
-topic = "Games"
+topic = "India"
 
 results = crew.kickoff(inputs={'topic':topic})
 
